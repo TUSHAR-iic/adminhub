105c7f905edc14afeddba1f7f6c7f5f7eacedf73
TUSHAR-iic <tushar.2023@iic.ac.in>
2024-07-12 11:26:50 +0530

    DRDO project code added

--- script.js
+++ script.js
@@ -1,76 +1,70 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const sidebar = document.querySelector('.sidebar');
+    const container = document.querySelector('.container');
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
-
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
+    hamburger.addEventListener('click', () => {
+        sidebar.classList.toggle('collapsed');
+        container.classList.toggle('collapsed');
+    });
 });
 
 
+// scroll to contact section
+document.getElementById("contact").addEventListener("click", function() {
+    document.querySelector(".footer-container").scrollIntoView({ behavior: 'smooth' });
+});
 
 
-// TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
+// content 1
+let number = document.getElementById('num');
+let count = 0;
+setInterval(() =>{
+    if (count == 65){
+        clearInterval();
+    }
+    else{
+    count ++;
+    number.innerHTML = count + "%";
+    }
+},30);
 
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
+// -----users count
+let number1 = document.getElementById("num-of-users");
+let count1 = 980;
+setInterval(() =>{
+    if (count1 == 1020){
+        clearInterval();
+    }
+    else{
+    count1 ++;
+    number1.innerHTML = count1;
+    }
+},30);
 
+// visitors count
+let number2 = document.getElementById("num-of-visitors");
+let count2 = 2794;
+setInterval(() =>{
+    if (count2 == 2834){
+        clearInterval();
+    }
+    else{
+    count2 ++;
+    number2.innerHTML = count2 ;
+    }
+},30);
 
-
-
-
-
-
-const searchButton = document.querySelector('#content nav form .form-input button');
-const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
-
-searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
-		e.preventDefault();
-		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
-			searchButtonIcon.classList.replace('bx-search', 'bx-x');
-		} else {
-			searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		}
-	}
-})
-
-
-
-
-
-if(window.innerWidth < 768) {
-	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
-	searchButtonIcon.classList.replace('bx-x', 'bx-search');
-	searchForm.classList.remove('show');
-}
-
-
-window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
-		searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		searchForm.classList.remove('show');
-	}
-})
-
-
-
-const switchMode = document.getElementById('switch-mode');
-
-switchMode.addEventListener('change', function () {
-	if(this.checked) {
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-	}
-})
+// Sales count
+let number3 = document.getElementById("num-of-sales");
+let count3 = 2898;
+setInterval(() =>{
+    if (count3 == 2938){
+        clearInterval();
+    }
+    else{
+    count3 ++;
+    number3.innerHTML = "$ "+count3 ;
+    }
+},30);
